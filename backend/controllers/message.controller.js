d9bfcfdbd1230c1bddfeb89dfb8e4d076356310f
Krasnov Mykola <krasnov.mykola.clg@chnu.edu.ua>
2024-05-28 11:09:15 +0300

    try to fix message bag

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -38,7 +38,7 @@ export const sendMessage = async (req, res) => {
         const receiverSocketId = getReceiverSocketId(receiverId);
 		if (receiverSocketId) {
 			// io.to(<socket_id>).emit() used to send events to specific client
-			io.to(receiverSocketId).emit("newMessage", newMessage);
+			io.to(receiverSocketId).emit("newMessage", {...newMessage, conversationsId: conversation._id});
 		}
 
         res.status(201).json(newMessage);
